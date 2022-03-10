c8fddd76e98b497d86c79d1e0c13d521fbb3c044
bindu1128 <himachowdary2811@gmail.com>
2022-03-10 07:38:09 -0600

    second code

---
+++ src/FirstProgram/CourseDetails.java
@@ -0,0 +1,19 @@
+package FirstProgram;
+
+public class CourseDetails {
+	
+	private void javacourse() {	
+	System.out.println("Java");
+		}
+
+	private void selenium() {
+		System.out.println("Automation");
+	}	
+	
+	public static void main(String[] args) {
+		CourseDetails cd=new CourseDetails();
+		cd.javacourse();
+		cd.selenium();
+		
+	}
+}
